d774ec99baa9d4985f13b6a754fa7f5ce7d749c1
Johannes Rieke <johannes.rieke@gmail.com>
2019-04-17 16:29:38 +0200

    Added simple neural network without mini-batching, gets around 80 percent accuracy

---
+++ main.py
@@ -0,0 +1,124 @@
+import mnist_loader
+import numpy as np
+import scipy as sp
+import scipy.special
+import sklearn.metrics
+import random
+
+sigmoid = sp.special.expit
+softmax = sp.special.softmax
+cross_entropy = sklearn.metrics.log_loss
+accuracy = sklearn.metrics.accuracy_score
+
+def d_sigmoid(x):
+    return sigmoid(x) * (1 - sigmoid(x))
+
+training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+
+len(training_data)
+
+W1 = np.random.randn(784, 30)
+W2 = np.random.randn(30, 10)
+b1 = np.random.rand(30)
+b2 = np.random.rand(10)
+
+training_data[0][0].shape, training_data[0][1].shape
+
+x = training_data[0][0].reshape(-1)
+y_true = training_data[0][1].reshape(-1)
+x.shape, y_true.shape
+
+def forward(x):
+    z1 = x @ W1 + b1
+    a1 = sigmoid(z1)
+
+    z2 = a1 @ W2 + b2
+    a2 = softmax(z2)  # TODO: When using mini batches, do axis=1.
+
+    return z1, a1, z2, a2
+
+[a.shape for a in forward(x)]
+
+def backprop_update(x, y_true, lr):
+    global b1, b2, W1, W2  # TODO: Remove once this is in network.
+    z1, a1, z2, a2 = forward(x)
+
+    # TODO: Does this work properly with n_samples?
+    # TODO: Maybe return this.
+    loss = cross_entropy(y_true, a2)
+    loss
+
+    # Compute errors for each layer.
+    e2 = a2 - y_true
+    e1 = d_sigmoid(z2) * e2 @ W2.T
+    #e1.shape, e2.shape
+
+    # Compute gradients of cost w.r.t. parameters.
+    grad_b1 = e1
+    grad_b2 = e2
+    grad_W1 = np.outer(x, e1)  # creates a matrix from two vectors
+    grad_W2 = np.outer(a1, e2)
+    #grad_W1.shape, grad_W2.shape, grad_b1.shape, grad_b2.shape
+
+    b1 -= lr * grad_b1
+    b2 -= lr * grad_b2
+    W1 -= lr * grad_W1
+    W2 -= lr * grad_W2
+
+    return a2
+
+
+def evaluate():
+    correct = 0
+    for i_sample, (x, y_true) in enumerate(validation_data):
+        x = x.reshape(-1)
+        y_true = y_true.reshape(-1)
+        _, _, _, y_pred = forward(x)
+        #print(x.shape, y_true, y_pred.shape)
+
+        # Targets in validation_data are labels, while in training_data, they are one-hot vectors.
+        if y_true[0] == y_pred.argmax():
+            correct += 1
+
+    avg_loss = np.NaN
+    avg_acc = correct / len(validation_data) * 100
+
+    print('Evaluating:\tLoss: {:.4f},\tAccuracy: {:.1f}%\n'.format(avg_loss, avg_acc))
+
+evaluate()
+
+def train_epoch(lr, shuffle=True):
+    if shuffle:
+        random.shuffle(training_data)
+    correct = 0
+    for i_sample, (x, y_true) in enumerate(training_data):
+        x = x.reshape(-1)
+        y_true = y_true.reshape(-1)
+        y_pred = backprop_update(x, y_true, lr)
+
+        if y_true.argmax() == y_pred.argmax():
+            correct += 1
+
+        if i_sample % 5000 == 1:
+            print('{} / {} samples - accuracy: {:.1f} %'.format(i_sample, len(training_data), correct / i_sample * 100))
+
+    avg_loss = np.NaN
+    avg_acc = correct / len(training_data) * 100
+    print('Average:\tLoss: {:.6f}\tAccuracy: {:.1f}%'.format(avg_loss, avg_acc))
+
+# Training loop.
+for epoch in range(1, 10):
+    print('Epoch', epoch)
+    train_epoch(lr=0.005)
+    evaluate()
+    print('-'*80)
+    print()
+
+
+# TODO: Implement mini-batching.
+def create_batches(data, batch_size, shuffle=True):
+    if shuffle:
+        random.shuffle(data)
+    return [data[k:k+batch_size] for k in range(0, len(data), batch_size)]
+
+#create_batches(training_data, 10)
