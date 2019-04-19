55e2e9c6fc62566fa457d4f7a4e96e176d434cfd
Johannes Rieke <johannes.rieke@gmail.com>
2019-04-19 21:17:50 +0200

    Added sign-symmetry and first version of target propagation (not working)

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ net = NeuralNet(num_hidden=30)
 # Training loop.
 for epoch in range(1, 20):
     print('Epoch', epoch)
-    train_epoch(net, training_data, lr=0.5)
+    train_epoch(net, training_data, lr=0.2)
     evaluate(net, validation_data)
     print('-'*80)
     print()
--- network.py
+++ network.py
@@ -1,6 +1,7 @@
 import numpy as np
 import scipy as sp
 import scipy.special
+import sklearn.metrics
 import random
 
 
@@ -10,6 +11,10 @@ softmax = sp.special.softmax
 
 def d_sigmoid(x):
     return sigmoid(x) * (1 - sigmoid(x))
+
+
+def mean_squared_error(y_pred, y_true):
+    return np.mean((y_pred - y_true)**2)
 
 
 def cross_entropy(y_pred, true_label):
@@ -23,24 +28,25 @@ class NeuralNet:
     def __init__(self, num_hidden=30):
         self.W1 = np.random.randn(784, num_hidden)
         self.W2 = np.random.randn(num_hidden, 10)
-        self.b1 = np.random.rand(num_hidden)
-        self.b2 = np.random.rand(10)
+        self.b1 = np.random.randn(num_hidden)
+        self.b2 = np.random.randn(10)
 
-        # Fixed random weights for Feedback Alignment.
-        self.B2 = np.random.randn(num_hidden, 10)
+        # Explicit feedback parameters for feedback alignment, sign symmetry and target propagation.
+        self.V2 = np.random.randn(10, num_hidden)
+        self.c2 = np.random.randn(num_hidden)
 
     def forward(self, x):
         # Hidden layer.
         z1 = x @ self.W1 + self.b1
-        a1 = sigmoid(z1)
+        h1 = sigmoid(z1)
 
         # Output layer.
-        z2 = a1 @ self.W2 + self.b2
-        a2 = sigmoid(z2)
+        z2 = h1 @ self.W2 + self.b2
+        h2 = sigmoid(z2)
         # To use softmax here, do this (when using mini-batches, add axis=1):
-        #a2 = softmax(z2)
+        #h2 = softmax(z2)
 
-        return z1, a1, z2, a2
+        return z1, h1, z2, h2
 
     # def backprop_update(self, batch, lr):
     #     # Average gradients over the mini batch.
@@ -74,19 +80,17 @@ class NeuralNet:
 
     def backprop_update(self, x, y_true, lr):
         # Run forward pass.
-        z1, a1, z2, a2 = self.forward(x)
+        z1, h1, z2, h2 = self.forward(x)
 
         # Compute errors for each layer (= gradient of cost w.r.t layer input).
-        e2 = d_sigmoid(z2) * (a2 - y_true)  # gradient through mean squared error loss and sigmoid
-        # To use cross-entropy here, do this (not sure if should be divided by len(a2) or not):
-        #e2 = (a2 - y_true) / len(a2)  # gradient through cross entropy loss and sigmoid
+        e2 = h2 - y_true  # gradient through cross entropy loss
         e1 = d_sigmoid(z1) * (e2 @ self.W2.T)  # gradient backpropagation
 
         # Compute gradients of cost w.r.t. parameters.
         grad_b1 = e1
         grad_b2 = e2
         grad_W1 = np.outer(x, e1)  # np.outer creates a matrix from two vectors
-        grad_W2 = np.outer(a1, e2)
+        grad_W2 = np.outer(h1, e2)
 
         # Update parameters.
         self.b1 -= lr * grad_b1
@@ -94,38 +98,38 @@ class NeuralNet:
         self.W1 -= lr * grad_W1
         self.W2 -= lr * grad_W2
 
-        return a2
+        return h2
 
     def final_layer_update(self, x, y_true, lr):
         # Run forward pass.
-        z1, a1, z2, a2 = self.forward(x)
+        z1, h1, z2, h2 = self.forward(x)
 
-        # Compute errors for each layer (= gradient of cost w.r.t layer input).
-        e2 = d_sigmoid(z2) * (a2 - y_true)  # gradient through cross entropy and softmax
+        # Compute error for final layer (= gradient of cost w.r.t layer input).
+        e2 = h2 - y_true  # gradient through cross entropy loss
 
         # Compute gradients of cost w.r.t. parameters.
         grad_b2 = e2
-        grad_W2 = np.outer(a1, e2)
+        grad_W2 = np.outer(h1, e2)
 
         # Update parameters.
         self.b2 -= lr * grad_b2
         self.W2 -= lr * grad_W2
 
-        return a2
+        return h2
 
     def feedback_alignment_update(self, x, y_true, lr):
         # Run forward pass.
-        z1, a1, z2, a2 = self.forward(x)
+        z1, h1, z2, h2 = self.forward(x)
 
         # Compute errors for each layer (= gradient of cost w.r.t layer input).
-        e2 = d_sigmoid(z2) * (a2 - y_true)  # gradient through cross entropy and softmax
-        e1 = d_sigmoid(z1) * (e2 @ self.B2.T)  # gradient backpropagation
+        e2 = h2 - y_true  # gradient through cross entropy loss
+        e1 = d_sigmoid(z1) * (e2 @ self.V2)  # gradient backpropagation
 
         # Compute gradients of cost w.r.t. parameters.
         grad_b1 = e1
         grad_b2 = e2
         grad_W1 = np.outer(x, e1)  # np.outer creates a matrix from two vectors
-        grad_W2 = np.outer(a1, e2)
+        grad_W2 = np.outer(h1, e2)
 
         # Update parameters.
         self.b1 -= lr * grad_b1
@@ -133,13 +137,68 @@ class NeuralNet:
         self.W1 -= lr * grad_W1
         self.W2 -= lr * grad_W2
 
-        return a2
+        return h2
+
+    def sign_symmetry_update(self, x, y_true, lr):
+        # Run forward pass.
+        z1, h1, z2, h2 = self.forward(x)
+
+        # Compute errors for each layer (= gradient of cost w.r.t layer input).
+        e2 = h2 - y_true  # gradient through cross entropy loss
+        e1 = d_sigmoid(z1) * (e2 @ (np.abs(self.V2) * np.sign(self.W2.T)))  # gradient backpropagation
+
+        # Using these errors, compute gradients of cost w.r.t. parameters.
+        grad_b1 = e1
+        grad_b2 = e2
+        grad_W1 = np.outer(x, e1)  # np.outer creates a matrix from two vectors
+        grad_W2 = np.outer(h1, e2)
+
+        # Update parameters.
+        self.b1 -= lr * grad_b1
+        self.b2 -= lr * grad_b2
+        self.W1 -= lr * grad_W1
+        self.W2 -= lr * grad_W2
+
+        return h2
 
     def weight_mirroring_update(self, x, y_true, lr):
         pass
 
-    def target_prop_update(self, x, y_true, lr):
-        pass
+    def target_prop_update(self, x, y_true, lr_final, lr_forward, lr_backward):
+        # Run forward pass.
+        # TODO: Rename a2, a2 to h1, h2 everywhere.
+        z1, h1, z2, h2 = self.forward(x)
+
+        # Compute targets for each layer.
+        h2_ = h2 - lr_final * (h2 - y_true)  # TODO: Why not use y_true here directly?
+        z1_ = h2_ @ self.V2 + self.c2  # TODO: Use h2 or h2_ here?
+        h1_ = sigmoid(z1_)
+
+        # Compute layerwise loss.
+        L2 = mean_squared_error(h2, h2_)
+        L1 = mean_squared_error(h1, h1_)
+        #print(L2, L1)
+
+        # Compute local gradients for forward parameters.
+        dL1_db1 = 2 * np.linalg.norm(h1 - h1_) * d_sigmoid(z1)
+        dL1_dW1 = 2 * np.linalg.norm(h1 - h1_) * np.outer(x, d_sigmoid(z1))  # TODO: Simply by reusing dL1_db1.
+        dL2_db2 = 2 * np.linalg.norm(h2 - h2_) * d_sigmoid(z2)
+        dL2_dW2 = 2 * np.linalg.norm(h2 - h2_) * np.outer(h1, d_sigmoid(z2))
+
+        # Compute local gradients for backward parameters.
+        dL1_dc2 = 2 * np.linalg.norm(h1 - h1_) * d_sigmoid(z1_)  # TODO: Verify that this is correct.
+        dL1_dV2 = 2 * np.linalg.norm(h1 - h1_) * np.outer(h2_, d_sigmoid(z1_))
+
+        # Update parameters.
+        self.b1 -= lr_forward * dL1_db1
+        self.W1 -= lr_forward * dL1_dW1
+        self.b2 -= lr_forward * dL2_db2
+        self.W2 -= lr_forward * dL2_dW2
+        self.c2 -= lr_backward * dL1_dc2
+        self.V2 -= lr_backward * dL1_dV2
+
+        return h2
+
 
     def equilibrium_prop_update(self, x, y_true, lr):
         pass
@@ -209,7 +268,7 @@ def train_epoch(net, training_data, lr, shuffle=True):
     for i_sample, (x, y_true) in enumerate(training_data):
         x = x.flatten()
         y_true = y_true.flatten()
-        y_pred = net.backprop_update(x, y_true, lr)
+        y_pred = net.target_prop_update(x, y_true, 3, 3, 3)#lr)
 
         true_label = y_true.argmax()  # target values in training_data they are one-hot vectors (in validation_data they are labels)
         running_loss += cross_entropy(y_pred, true_label)
